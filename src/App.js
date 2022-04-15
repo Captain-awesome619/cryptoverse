954516d7fa5a6d36896b883ef475802c15649376
unknown <ogunsolatoluwalase@gmail.com>
2022-04-15 21:07:28 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ const App = () => (
         </div>
       </Layout>
       <div className="footer">
-        <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>Copyright © 2021
+        <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>Copyright © 2022
           <Link to="/">
             Cryptoverse Inc.
           </Link> <br />
