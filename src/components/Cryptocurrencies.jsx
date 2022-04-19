935e8875d607bea3b7f38cc4999a61dfa8a5860b
unknown <ogunsolatoluwalase@gmail.com>
2022-04-20 00:01:10 +0100

    first commit

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import millify from 'millify';
 import { Link } from 'react-router-dom';
 import { Card, Row, Col, Input } from 'antd';
-
+import './crypto.css';
 import { useGetCryptosQuery } from '../services/cryptoApi';
 import Loader from './Loader';
 
@@ -44,17 +44,16 @@ const Cryptocurrencies = ({ simplified }) => {
 
             {/* Note: Change currency.id to currency.uuid  */}
             <Link key={currency.uuid} to={`/crypto/${currency.uuid}`}>
-              <Card
+              <Card 
+                className="hove"
                 title={`${currency.rank}. ${currency.name}`}
                 extra={<img className="crypto-image" src={currency.iconUrl} />}
-                hoverable
               >
                 <p>Price: {millify(currency.price)}</p>
                 <p>Market Cap: {millify(currency.marketCap)}</p>
                 <p>Daily Change: {currency.change}%</p>
               </Card>
             </Link>
-            <Link key={currency.uuid} to={`/crypto/${currency.uuid}`}><button type="button"> see details </button></Link>
           </Col>
         ))}
       </Row>
